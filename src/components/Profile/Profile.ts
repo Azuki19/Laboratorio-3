444af06b0bad22e3a99751a039b8e01b2cec7324
Azuki19 <analucia27259@gmail.com>
2024-03-09 23:35:21 -0500

    .

---
+++ src/components/Profile/Profile.ts
@@ -0,0 +1,94 @@
+export enum Attribute {
+	'uid' = 'uid',
+	'name' = 'name',
+	'image' = 'image',
+	'age' = 'age',
+	'gender' = 'gender',
+	'area' = 'area',
+	'position' = 'position',
+	'timeInCompany' = 'timeInCompany',
+	'experience' = 'experience',
+}
+
+class Profile extends HTMLElement {
+	uid?: number;
+	name?: string;
+	image?: string;
+	age?: number;
+	gender?: string;
+	area?: string;
+	position?: string;
+	timeInCompany?: number;
+	experience?: number;
+
+	static get observedAttributes() {
+		const attrs: Record<Attribute, null> = {
+			uid: null,
+			name: null,
+			image: null,
+			age: null,
+			gender: null,
+			area: null,
+			position: null,
+			timeInCompany: null,
+			experience: null,
+		};
+		return Object.keys(attrs);
+	}
+
+	attributeChangedCallback(propName: Attribute, oldValue: string | undefined, newValue: string | undefined) {
+		switch (propName) {
+			case Attribute.uid:
+				this.uid = newValue ? Number(newValue) : undefined;
+				break;
+
+			case Attribute.age:
+				this.age = newValue ? Number(newValue) : undefined;
+				break;
+
+			case Attribute.timeInCompany:
+				this.timeInCompany = newValue ? Number(newValue) : undefined;
+				break;
+
+			case Attribute.experience:
+				this.experience = newValue ? Number(newValue) : undefined;
+				break;
+
+			default:
+				this[propName] = newValue;
+				break;
+		}
+
+		this.render();
+	}
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="./src/components/Profile/Profile.css">
+          <section>
+
+          <img src="${this.image}"></img>
+          <h1>"${this.name}"</h1>
+          <p>"Id: ${this.uid}"<p>
+          <p>"${this.gender}"<p>
+          <p>"Area: ${this.area}"<p>
+          <p>"Time in company: ${this.timeInCompany}"<p>
+          <p>"Experience: ${this.experience}"<p>
+          </section>
+          `;
+		}
+	}
+}
+
+customElements.define('my-profile', Profile);
+export default Profile;
---
+++ src/indexAbuelo.ts
@@ -0,0 +1,45 @@
+import * as components from './components/indexPadre';
+import Profile, { Attribute } from './components/Profile/Profile';
+import { data } from './data/data';
+
+class AppContainer extends HTMLElement {
+	profiles: Profile[] = [];
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+
+		data.forEach((user) => {
+			const profileCard = this.ownerDocument.createElement('my-profile') as Profile;
+			profileCard.setAttribute(Attribute.name, user.name);
+			profileCard.setAttribute(Attribute.uid, String(user.id));
+			profileCard.setAttribute(Attribute.image, user.image);
+			profileCard.setAttribute(Attribute.age, String(user.age));
+			profileCard.setAttribute(Attribute.gender, user.gender);
+			profileCard.setAttribute(Attribute.area, user.jobDetails.area);
+			profileCard.setAttribute(Attribute.position, user.jobDetails.position);
+			profileCard.setAttribute(Attribute.timeInCompany, String(user.jobDetails.timeInCompany));
+			profileCard.setAttribute(Attribute.experience, String(user.jobDetails.experience));
+
+			this.profiles.push(profileCard);
+		});
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+            <h1>PROFILES</h1>
+            <h2>company</h2>`;
+
+			this.profiles.forEach((profile) => {
+				this.shadowRoot?.appendChild(profile);
+			});
+		}
+	}
+}
+
+customElements.define('app-container', AppContainer);
